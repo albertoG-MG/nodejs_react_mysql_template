bbf83f0892dc3190e1a9a499cd00ff7a11ab271d
FenrisWolf <albertomg@hotmail.com>
2024-08-31 11:32:25 -0600

    Se crea el middleware de authentificacion para el usuario

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
+const authMiddleware = require('../middleware/authMiddleware');
 
-router.get("/getusers", userController.getUsers);
+router.get("/getusers", authMiddleware, userController.getUsers);
 
 module.exports = router;
