53793224b479c7122f021a484090041ad83fb335
FenrisWolf <albertomg@hotmail.com>
2024-10-16 17:16:35 -0600

    Se crea el campo de la contraseña y se crea su api

--- frontend/src/pages/usuarios/Editarusuarios.js
+++ frontend/src/pages/usuarios/Editarusuarios.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect, useRef  } from 'react';
 import { useNavigate, useParams  } from 'react-router-dom';
 import useApiChecarUsuarioBack from '../../services/usuarios/useApiChecarUsuarioBack';
 import useApiChecarEditUsuario from '../../services/usuarios/form_services/validacion/useApiChecarEditUsuario';
+import useApiChecarEditPassword from '../../services/usuarios/form_services/validacion/useApiChecarEditPassword';
 import Swal from 'sweetalert2';
 
 export default function EditarUsuarios() {
@@ -89,10 +90,13 @@ export default function EditarUsuarios() {
                 clearTimeout(debounceRef.current);
                 debounceRef.current = setTimeout(() => verificarUsuario(value, id), 500);
 
-            } 
+            } else if(name === 'password'){
+                clearTimeout(debounceRef.current);
+                debounceRef.current = setTimeout(() => verificarPassword(value, id), 500);
+            }
         } catch (err) {
             console.error('Error en la llamada a la API:', err);
-            setErrores((prev) => ({ ...prev, username: ''}));
+            setErrores((prev) => ({ ...prev, username: '', password: ''}));
         }
     }
 
@@ -112,6 +116,22 @@ export default function EditarUsuarios() {
         }
     };
 
+    const verificarPassword = async (password, id) => {
+        try {
+            const { success, message } = await useApiChecarEditPassword(token, { password }, id);
+            setErrores((prev) => ({
+                ...prev,
+                password: success ? prev.password : message
+            }));
+        } catch (err) {
+            console.error('Error en la llamada a la API:', err);
+            setErrores((prev) => ({
+                ...prev,
+                password: prev.password || 'Error al verificar la contraseña. Inténtalo nuevamente.'
+            }));
+        }
+    };
+
     const validar = (Campos, campo = "", esEnvio = false) => {
         const errores = {};
     
@@ -123,6 +143,16 @@ export default function EditarUsuarios() {
                 errores.username = 'El usuario debe de tener como mínimo 5 caracteres.';
             } else if (!/^(?=[a-zA-Z0-9._]{4,30}$)(?!.*[_.]{2})[^_.].*[^_.]$/.test(Campos.username)) {
                 errores.username = 'El usuario debe de tener entre 4 y 30 caracteres, sin puntos o guiones bajos consecutivos, y no puede empezar o terminar con un punto o guion bajo.';
+            }
+        }
+
+        if(campo === "password" || esEnvio){
+            if (!Campos.password) {
+                errores.password = 'La contraseña es requerida.';
+            } else if (Campos.password && Campos.password.length < 8) {
+                errores.password = 'La contraseña debe de tener como mínimo 8 caracteres.';
+            } else if (!/^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[!@#$%&*])[a-zA-Z0-9!@#$%&*]+$/.test(Campos.password)) {
+                errores.password = 'La contraseña debe contener al menos una letra mayúscula, una letra minúscula, un número y un carácter especial (!@#$%&*).';
             }
         }
     
@@ -156,6 +186,25 @@ export default function EditarUsuarios() {
                         />
                     </div>
                     {Errores.username && <p className="text-red-500">{Errores.username}</p>}
+                </div>
+                <div className="grid grid-cols-1 mt-5 mx-7">
+                    <label className="text-[#64748b] font-semibold mb-2">Ingrese la contraseña</label>
+                    <div className="group flex">
+                        <div className="w-10 z-[1] pl-1 text-center pointer-events-none flex items-center justify-center">
+                            <svg class="w-5 h-5 text-gray-500" viewBox="0 0 24 24">
+                                <path fill="currentColor" d="M12,17A2,2 0 0,0 14,15C14,13.89 13.1,13 12,13A2,2 0 0,0 10,15A2,2 0 0,0 12,17M18,8A2,2 0 0,1 20,10V20A2,2 0 0,1 18,22H6A2,2 0 0,1 4,20V10C4,8.89 4.9,8 6,8H7V6A5,5 0 0,1 12,1A5,5 0 0,1 17,6V8H18M12,3A3,3 0 0,0 9,6V8H15V6A3,3 0 0,0 12,3Z"></path>
+                            </svg>
+                        </div>
+                        <input
+                            className="w-full -ml-10 pl-10 py-2 h-11 border rounded-md border-[#d1d5db] focus:ring-2 focus:ring-indigo-600 focus:outline-none"
+                            type="password"
+                            name="password"
+                            placeholder="Contraseña"
+                            value={Campos.password}
+                            onChange={manejarCambio}
+                        />
+                    </div>
+                    {Errores.password && <p className="text-red-500">{Errores.password}</p>}
                 </div>
             </form>
         </>
---
+++ frontend/src/services/usuarios/form_services/validacion/useApiChecarEditPassword.js
@@ -0,0 +1,38 @@
+import axios from 'axios';
+
+const API_URL="http://localhost:8080/api/users/checareditpassword";
+
+export default async function useApiChecarEditPassword(token, password, id){
+
+    try{
+
+        const response = await axios.get(API_URL, {
+            headers:{
+                'Authorization': `Bearer ${token}`
+            },
+            params:{
+                id:id,
+                password:password
+            }
+        });
+
+        return response.data;
+    }catch(error){
+        console.error("Error al hacer la solicitud de password" +error);
+
+        let errorMessage = "Ocurrió un error inesperado";
+
+        if(error.response){
+            errorMessage = error.response.data.message || "Ocurrió un error en la conexión de la API";
+        }else if(error.request){
+            errorMessage = "No se recibió respuesta del servidor";
+        }else{
+            errorMessage = "Error en la configuración de la solicitud";
+        }
+
+        return {
+            success: false,
+            message: errorMessage
+        }
+    }
+}
