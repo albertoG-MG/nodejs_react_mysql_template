5b8bb9b6c091df0a2cf26618dced7230a5a37c40
FenrisWolf <albertomg@hotmail.com>
2024-10-21 11:38:48 -0600

    Se crea el metodo para enviar el formulario para editar

--- frontend/src/pages/usuarios/Editarusuarios.js
+++ frontend/src/pages/usuarios/Editarusuarios.js
@@ -36,6 +36,7 @@ export default function EditarUsuarios() {
     const [Errores, setErrores] = useState({});
     const debounceRef = useRef(null);
     const [borrado, setBorrado] = useState('');
+    const [cargandoform, setCargandoform] = useState(false);
 
     //Traer valores de la base de datos
     const checarUserDatos = async () => {
@@ -268,12 +269,42 @@ export default function EditarUsuarios() {
 
     const ManejarBorrado = (borrar) => {
         setBorrado(borrar);
-    };       
+    };
+
+    const manejarEnvio = async (e) => {
+        e.preventDefault();
+
+        const erroresEnTiempoReal = { ...Errores };
+        const erroresDeEnvio = validar(Campos, "", true);
+
+        const erroresCombinados = { ...erroresEnTiempoReal, ...erroresDeEnvio };
+        setErrores(erroresCombinados);
+
+        const erroresFiltrados = Object.fromEntries(
+            Object.entries(erroresCombinados).filter(([_, value]) => value)
+        );
+
+        if (Object.keys(erroresFiltrados).length === 0) {
+            console.log('Formulario enviado con éxito', Campos);
+            setCargandoform(true);
+
+            const formData = new FormData();
+            for (const [key, value] of Object.entries(Campos)) {
+                formData.append(key, value);
+            }
+
+            const token = localStorage.getItem('token');
+            
+
+        } else {
+            console.log('Hay errores en el formulario', erroresFiltrados);
+        }
+    };
 
     return (
         <>
             <h1 className="text-3xl font-semibold sm:text-5xl lg:text-6xl mb-5 mx-7">Editar Usuario</h1>
-            <form>
+            <form onSubmit={manejarEnvio}>
                 <div className="grid grid-cols-1 mt-5 mx-7">
                     <label className="text-[#64748b] font-semibold mb-2">Ingrese el usuario</label>
                     <div className="group flex">
@@ -433,6 +464,25 @@ export default function EditarUsuarios() {
                         />
                     )}
                     {Errores.foto && <p className="text-red-500">{Errores.foto}</p>}
+                </div>
+                <div className="mt-8 h-px bg-slate-200"></div>
+                <div className="text-right">
+                    {cargandoform ? (
+                        <button disabled type="button" class="bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline mt-5 inline-flex items-center">
+                        <svg aria-hidden="true" role="status" class="inline w-4 h-4 me-3 text-white animate-spin" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="#E5E7EB"/>
+                        <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentColor"/>
+                        </svg>
+                        Cargando...
+                        </button>
+                    ) : (
+                        <button
+                            className="bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline mt-5"
+                            type="submit"
+                        >
+                            Editar Usuario
+                        </button>
+                    )}
                 </div>
             </form>
         </>
